c94c14fd225bd1d1ee2bb3ca29af45118d9ce7ad
git-pusher <rachel.palomeque@gmail.com>
2019-08-22 14:16:28 -0500

    ticket validation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+
+  //guardo el Jquery en una variable para facilitar su uso
   let description = $('#description')
   let carbs = $('#carbs')
   let calories = $('#calories')
   let protein = $('#protein')
+
+  //valida si está vacío o no el campo
+  const validateInputs = () => {
+    // if(description.val() === '') {
+    //   description.addClass('is-invalid')
+    // }
+    //cambiamos a operador ternario para reducir el código
+    description.val() ? '' : description.addClass('is-invalid')
+    calories.val() ? '' : calories.addClass('is-invalid')
+    carbs.val() ? '' : carbs.addClass('is-invalid')
+    protein.val() ? '' : protein.addClass('is-invalid')
+  }
